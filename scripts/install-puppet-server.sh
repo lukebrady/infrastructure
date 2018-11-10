b08d0eafa1936a1f6bcdebe182ab8bbdec1236ad
Luke Brady <lbrad23105@gmail.com>
2018-11-10 11:59:31 -0500

    Added install script.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

---
+++ scripts/install-puppet-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $(whoami) != "root" ]]; then
+	echo "This script must be run as root.";
+	exit 1;
+fi
+
+# Install the Puppet 6 Repo. This is for CentOS 7.
+sudo rpm -Uvh https://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
+sudo yum install -y puppetserver facter
